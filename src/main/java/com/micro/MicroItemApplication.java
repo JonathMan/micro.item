5e7cc2f031e164ae2e209de73e1841b1e9d29acd
Juan Escobar <juand-escobart@unilibre.edu.co>
2019-10-12 16:48:01 -0500

    Correciones

--- src/main/java/com/micro/MicroItemApplication.java
+++ src/main/java/com/micro/MicroItemApplication.java
@@ -2,8 +2,10 @@ package com.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroItemApplication {
 
 	public static void main(String[] args) {
